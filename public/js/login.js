8a8e8b26be84a2e10aa2028c3c6c4aa0e096446d
shikkandar <mohammedshik3@gmail.com>
2023-08-04 13:12:46 +0530

    authentication

--- public/js/login.js
+++ public/js/login.js
@@ -1,34 +1,8 @@
-document.getElementById("submitBtn").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    var username = document.getElementById("usernameInput").value;
-    var password = document.getElementById("passwordInput").value;
-
-    if (username === "admin" && password === "admin1234") {
-      window.location.href = "/admissionPage1";
-    } else {
-      alert("Incorrect username or password. Please try again.");
-    }
-  });
-  
-document.addEventListener("DOMContentLoaded", function() {
-  var aboutLink = document.querySelector('li[onclick="about"] > a');
-  var aboutDropdown = document.querySelector('.about');
-
-  aboutLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    aboutDropdown.classList.toggle("show");
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('signup-form');
+  form.classList.add('show');
 });
-let container = document.getElementById('container')
-
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
-
-setTimeout(() => {
-	container.classList.add('sign-in')
-}, 200)
-
-
-  
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('login-form');
+  form.classList.add('show');
+});
